ef49f0a4503ccd8322900bcf8a438c63b45d924a
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-04-02 12:57:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 input.onButtonPressed(Button.A, function () {
+    // runs inside code 5 times while increasing "a" by 1 each time
     for (let a = 0; a <= 4; a++) {
-        for (let index = 0; index <= 4; index++) {
-            led.toggle(a, index)
+        // runs inside code 5 times while increasing "b" by 1 each time
+        for (let b = 0; b <= 4; b++) {
+            // toggles leds at (a,b)
+            led.toggle(a, b)
+            // pauses for 100ms
             basic.pause(100)
-            led.toggle(index, a)
+            // plots leds at (b,a)
+            led.plot(b, a)
+            // pauses for 100ms
             basic.pause(100)
         }
     }
